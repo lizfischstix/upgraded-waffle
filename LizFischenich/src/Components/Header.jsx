57d0e6e1e52af9ac109122d126886104162fb92e
Liz FIschenich <Liz.Devlin85@gmail.com>
2024-04-25 13:08:12 -0500

    frame set up. emailjs installed and contact form componant started. Nav started. TODO route nav, test contact, add content, make it pretty

--- LizFischenich/src/Components/Header.jsx
+++ LizFischenich/src/Components/Header.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import  { Grid, Box, Button, TextField, Typography, Container } from '@mui/material';
+
 export default function Header(){
     return (
-        <>
-       <h1> THIS IS THE HEADER</h1>
-        </>
+      <>
+      <Box>
+        <Typography align="center" fontSize='5rem' style={{fontFamily:'Amatic sc'}} >Liz Fischenich</Typography>
+      </Box>
+      </>
     );
 }
--- LizFischenich/src/Components/Nav.jsx
+++ LizFischenich/src/Components/Nav.jsx
@@ -1,5 +1,44 @@
-export default function Nav(){
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RoofingIcon from '@mui/icons-material/Roofing';
+import FingerprintIcon from '@mui/icons-material/Fingerprint';
+import FolderSpecialIcon from '@mui/icons-material/FolderSpecial';
+import SendIcon from '@mui/icons-material/Send';
+
+
+export default function Nav() {
+    const [value, setValue] = React.useState('home');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-    <h1>Nav Tabs to come</h1>
-)
+    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+    
+      <BottomNavigationAction
+        label="Liz Fischenich"
+        value="home"
+        icon={<RoofingIcon />}
+       />
+      
+      
+      <BottomNavigationAction
+        label="About"
+        value="about"
+        icon={<FingerprintIcon />}
+      />
+      
+ 
+      <BottomNavigationAction
+        label="Portfolio"
+        value="portfolio"
+        icon={<FolderSpecialIcon />}
+      />
+    
+        <BottomNavigationAction label="Contact Liz" value="contact" icon={<SendIcon />} />
+    < /BottomNavigation >
+  );
 }
